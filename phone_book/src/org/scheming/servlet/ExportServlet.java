8bff92778175daf88c594e342f06a6d6e7cc2baf
Scheming <1055062460@qq.com>
2015-05-08 09:25:04 +0800

    加入导出表格功能

---
+++ phone_book/src/org/scheming/servlet/ExportServlet.java
@@ -0,0 +1,99 @@
+package org.scheming.servlet;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.scheming.model.User;
+
+@WebServlet("/base/ExportServlet")
+public class ExportServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExportServlet() {
+		super();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		List<User> lists = (List<User>) getServletContext().getAttribute(
+				request.getParameter("id"));
+
+		response.setContentType("application/vnd.ms-excel");
+		response.setHeader("Content-Disposition", "inline; filename="
+				+ "phone_book.xls");
+
+		OutputStream outputStream = response.getOutputStream();
+		doExcle(outputStream, lists);
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	private void doExcle(OutputStream outputStream, List<User> lists) {
+
+		String[] column = { "学号", "姓名", "电话", "QQ" };
+
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet(lists.get(0).getCla());
+		sheet.setDefaultColumnWidth(20);
+
+		for (int i = 0; i < lists.size() + 1; i++) {
+			HSSFRow row = sheet.createRow(i);
+			if (i == 0) {
+				// row.setHeight((short) 10);
+				for (int j = 0; j < column.length; j++) {
+					HSSFCell cell = row.createCell(j);
+					cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+					cell.setCellValue(column[j]);
+				}
+			} else {
+				User user = lists.get(i - 1);
+				for (int j = 0; j < column.length; j++) {
+					HSSFCell cell = row.createCell(j);
+					cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+					switch (j) {
+					case 0:
+						cell.setCellValue(user.getId());
+						break;
+					case 1:
+						cell.setCellValue(user.getName());
+						break;
+					case 2:
+						cell.setCellValue(user.getTel());
+						break;
+					case 3:
+						cell.setCellValue(user.getQq());
+						break;
+					}
+				}
+			}
+
+		}
+
+		try {
+			workbook.write(outputStream);
+			outputStream.flush();
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
